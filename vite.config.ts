0308251beb5e62cb04db5b3bc73438eb375ffc7c
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2026-09-16 00:11:28 +0800

    chore: 工程化升级

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,16 @@
 import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
-  test: {
-    include: ['packages/*/src/**/*.test.ts'],
-  },
   staged: {
-    '**/*.{js,jsx,ts,tsx}': 'vp check',
-    '**/*.{html,css,md,json}': 'vp fmt',
+    '**/*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}': 'vp check',
+    '**/*.{html,css,scss,less,md,mdx,json,jsonc,json5,yaml,yml,toml,graphql,gql,vue}': 'vp fmt',
   },
   lint: {
     options: {
       typeAware: true,
       typeCheck: true,
     },
-    plugins: ['typescript'],
-    env: {
-      node: true,
-    },
+    plugins: ['typescript', 'unicorn', 'oxc', 'import'],
     rules: {
       'no-unused-vars': [
         'error',
@@ -27,11 +21,39 @@ export default defineConfig({
           caughtErrorsIgnorePattern: '^_',
         },
       ],
+      'no-fallthrough': 'error',
     },
   },
   fmt: {
+    ignorePatterns: [
+      '**/.DS_Store',
+      '**/.eslintcache',
+      '**/yarn.lock',
+      '**/pnpm-lock.yaml',
+      '**/package-lock.json',
+      '**/node_modules',
+      '**/.husky',
+      '**/*.log',
+      '**/*.ico',
+      '**/*.svg',
+      '**/*.png',
+      '**/*ignore',
+      '**/.editorconfig',
+      '**/CHANGELOG.md',
+      '**/build',
+      '**/coverage',
+      '**/dist',
+      '**/.next',
+      '**/.umi',
+      '**/.umi-test',
+      '**/.umi-production',
+    ],
     printWidth: 120,
     proseWrap: 'never',
     singleQuote: true,
+    sortPackageJson: true,
+    sortImports: {
+      newlinesBetween: false,
+    },
   },
 });
